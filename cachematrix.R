15f979af82d1167a18275e5a683ee5a3aea89ea0
hussamrefaat <hussamrefaat@hotmail.com>
2016-10-10 18:46:16 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function create a matrix and set inverse in parent enviroments , get inverse , set and get the value
 
 makeCacheMatrix <- function(x = matrix()) {
         i<-NULL
@@ -19,20 +19,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## this function is to calculate a matrix inverse using function Solve ,if inverse value is already set gets it from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
         z<-x$getinverse()
-        
+    ##check if there is value already cached for matrix inverse     
         if(!is.null(z))  {
                 
                 message("getting cached data")
                 return(z)
         }
         else{
-                
+       ##no cached value so it calculates the inverse save it in variable i and then save it using setinverse defined in the solve cache matrix function         
                B<- x$get() 
                i<-solve(B)
                x$setinverse(i)
